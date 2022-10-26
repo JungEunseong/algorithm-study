03b4173bf400edb127de3c9a3f020cc62f68c77a
EunSung <82389915+EnSung@users.noreply.github.com>
2022-10-26 23:22:22 +0900

    dynamic array

--- rookiss_structure/Rookiss_Algorithm/Rookiss_Algorithm/Board.cs
+++ rookiss_structure/Rookiss_Algorithm/Rookiss_Algorithm/Board.cs
@@ -6,16 +6,65 @@ using System.Threading.Tasks;
 
 namespace Rookiss_Algorithm
 {
+
+    class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+        
+        public int Count; // 실제로 사용중인 데이터 개수
+        public int Capacity { get { return _data.Length; } } // 예약된 데이터 개수
+
+        public void Add(T item)
+        {
+            // 1. 공간이 충분한지 확인
+            if(Count >= Capacity)
+            {
+                // 공간을 다시 늘려서 확보한다
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;                
+            }
+
+
+            // 2. 공간에 데이터를 넣는다.
+            _data[Count] = item;
+            Count++;
+        }
+
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+                _data[i] = _data[i+1];
+            _data[Count - 1] = default(T);
+            Count--;
+        }
+    }
     class Board
     {
         public int[] _data = new int[25]; // 배열
-        public List<int> _data2 = new List<int>(); // 동적 배열
+        public MyList<int> _data2 = new MyList<int>(); // 동적 배열
         public LinkedList<int> _data3 = new LinkedList<int>(); // 연결 리스트
         
         
         public void Initialize()
         {
-             _data2.Add()     
+            _data2.Add(101);     
+            _data2.Add(102);     
+            _data2.Add(103);     
+            _data2.Add(104);     
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
         }
 
 
