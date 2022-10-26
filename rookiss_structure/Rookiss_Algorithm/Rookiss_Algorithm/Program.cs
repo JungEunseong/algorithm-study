e15b00ba7cd51db9f10338f688c8f3e861755444
EunSung <82389915+EnSung@users.noreply.github.com>
2022-10-26 22:33:48 +0900

    .
    .

---
+++ rookiss_structure/Rookiss_Algorithm/Rookiss_Algorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿using Rookiss_Algorithm;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Board board = new Board();
+        board.Initialize();
+        Console.CursorVisible = false;
+
+        const int WAIT_TICK = 1000 / 30;
+        const char CIRCLE = '\u25cf';
+        int lastTick = 0;
+        while (true)
+        {
+            #region 프레임 관리
+            int currentTick = System.Environment.TickCount;
+
+            if (currentTick - lastTick < WAIT_TICK)
+                continue;
+            lastTick = currentTick;
+            #endregion
+
+            // 입력
+            // 로직
+            // 렌더링
+
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < 25; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(CIRCLE);
+                }
+
+                Console.WriteLine();
+            }
+            
+            
+        }
+    }
+}
