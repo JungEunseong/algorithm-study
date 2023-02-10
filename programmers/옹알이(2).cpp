58d99b5001aad700e791f4dbadcf4855fd31fbc1
EnSung <dmstjd0911@naver.com>
2023-02-10 15:03:30 +0900

    Create 옹알이(2).cpp

---
+++ programmers/옹알이(2).cpp
@@ -0,0 +1,31 @@
+int solution(vector<string> babbling) {
+
+    string canSay[] = { "aya", "ye", "woo", "ma" };
+    int answer = 0;
+
+    for (auto bab = babbling.begin(); bab != babbling.end(); ++bab)
+    {
+        string b = *bab;
+        for (int i = 0; i < sizeof(canSay) / sizeof(*canSay); i++)
+        {
+            while (b.find(canSay[i]) != string::npos)
+                b.replace(b.find(canSay[i]), canSay[i].length(), ")");
+
+            if (b.find("))") != string::npos)
+                break;
+            else {
+                while (b.find(")") != string::npos)
+                    b.replace(b.find(")"), 1, " ");
+            }
+        }
+
+        while (b.find(" ") != string::npos)
+            b.replace(b.find(" "), 1, "");
+        if (b == "") {
+            answer++;
+        }
+
+
+    }
+    return answer;
+}
