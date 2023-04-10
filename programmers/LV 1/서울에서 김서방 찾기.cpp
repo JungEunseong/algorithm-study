f662fb77c63976be4b4c392872afe7277e8ca4af
정은성 <82389915+EnSung@users.noreply.github.com>
2023-04-10 09:51:15 +0900

    Create 서울에서 김서방 찾기.cpp

---
+++ programmers/LV 1/서울에서 김서방 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int idx = 0;
+    
+    idx = find(seoul.begin(),seoul.end(),"Kim") - seoul.begin();
+    
+    answer = "김서방은 " + to_string(idx) + "에 있다";
+    return answer;
+}
