b1c7c9390bf23f8c3ae75e1d6f2928d8f115f51e
정은성 <82389915+JungEunseong@users.noreply.github.com>
2023-06-16 10:28:27 +0900

    [Silver IV] Title: 캠프파이어, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3018. 캠프파이어/캠프파이어.cc
@@ -0,0 +1,65 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <numeric>
+#include <sstream>
+
+using namespace std;
+
+
+int main() {
+	int N, E  = 0;
+	long long allSong = 0;
+	long long songBit = 1;
+	cin >> N;
+	cin >> E;
+
+	vector<long long> hearedSong(N + 1);
+
+
+	for (int i = 0; i < E; i++) {
+
+		int num;
+		cin >> num;
+
+		vector<int> people;
+		for (int j = 0; j < num; j++) {
+			int personId;
+			cin >> personId;
+			people.push_back(personId);
+		}
+
+		bool seonyeong = find(people.begin(), people.end(), 1) != people.end();
+
+		// 선영이가 있을 때
+		if (seonyeong) {
+
+			allSong |= (allSong << 1 | 1);
+
+			for (int j = 0; j < num; j++)
+				hearedSong[people[j]] |= songBit;
+
+			songBit = songBit << 1;
+
+		}
+		else {
+			long long song_union = 0;
+			for (int j = 0; j < people.size(); j++)
+				song_union |= hearedSong[people[j]];
+
+			for (int j = 0; j < people.size(); j++)
+				hearedSong[people[j]] = song_union;
+		}
+	}
+
+	
+
+	for (int i = 0; i < hearedSong.size(); i++)
+		if ((hearedSong[i] & allSong) == allSong)
+			cout << i << endl;
+
+
+	return 0;
+}	
