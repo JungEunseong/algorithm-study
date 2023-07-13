27c81af005f890c57e39871a6e356c299ecfe5c6
정은성 <82389915+JungEunseong@users.noreply.github.com>
2023-07-13 18:52:32 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,42 +1,9 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-	queue<pair<int,int>> current_truck; // 나갈 시간, 트럭 무게
-	int current_pass_truck = 0;
-	int total_weight = 0;
-	int nextTruckIdx = 0;
-	int time = 1;
-
-	while (current_pass_truck < truck_weights.size()) {
-
-
-		if (nextTruckIdx < truck_weights.size()) {
-
-			int next_truck_weight = truck_weights[nextTruckIdx];
-
-			if (total_weight + next_truck_weight <= weight) {
-				current_truck.push(make_pair(time - 1  + bridge_length, next_truck_weight));
-				nextTruckIdx++;
-				total_weight += next_truck_weight;
-			}
-
-		}
-
-		auto front_truck = current_truck.front();
-
-		if (front_truck.first <= time) {
-			current_truck.pop();
-			total_weight -= front_truck.second;
-			current_pass_truck++;
-		}
-
-
-		time++;
-	}
-	
-	return time;
+    int answer = 0;
+    return answer;
 }
