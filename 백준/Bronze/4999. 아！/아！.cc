a0117f084a1f6ee9aaa75e143e4d4bc6d4b3fc13
정은성 <82389915+JungEunseong@users.noreply.github.com>
2023-09-01 10:47:29 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+
+	string Jeahwan;
+	string Doctor;
+	int jeahwanCount = 0, doctorCount = 0;
+	cin >> Jeahwan;
+	cin >> Doctor;
+	
+	for (int i = 0; i < Jeahwan.length(); i++) {
+		if (Jeahwan[i] == 'a') {
+			jeahwanCount++;
+		}
+	}
+
+	for (int i = 0; i < Doctor.length(); i++) {
+		if (Doctor[i] == 'a') {
+			doctorCount++;
+		}
+	}
+
+	if (jeahwanCount >= doctorCount || Jeahwan.length() > Doctor.length())
+		cout << "go" << endl;
+	else
+		cout << "no" << endl;
+}
