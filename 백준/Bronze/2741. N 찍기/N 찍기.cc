e574c82b191a89d15a295b686d2f8bac2a4728db
정은성 <82389915+JungEunseong@users.noreply.github.com>
2023-09-10 19:40:35 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int num = 0;
+
+	scanf("%d",&num);
+
+
+	for (int i = 1; i <= num; i++)
+		printf("%d\n", i);
+
+
+}
