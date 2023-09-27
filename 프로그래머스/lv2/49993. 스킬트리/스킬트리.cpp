b26a1fad5b3abf5ceb288c5c9ccaabbfa8086e3f
정은성 <82389915+JungEunseong@users.noreply.github.com>
2023-09-27 09:07:41 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.cpp
@@ -1,35 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+
 int solution(string skill, vector<string> skill_trees) {
-    int answer = 0;
-    
-    vector<char> skillTree;
-    int nextSkilltreeIdx =0;
-
-    for (char& s : skill)
-        skillTree.push_back(s);
-
-    for (string& currentSkill : skill_trees) {
-        bool CanSkillTree = true;
-        for (char& s : currentSkill) {
-            auto isSkillInTree = find(skillTree.begin(), skillTree.end(), s);
-            if (isSkillInTree != skillTree.end()) {
-                if (skillTree[nextSkilltreeIdx] != s) {
-                    CanSkillTree = false;
-                    break;
-                }
-                else nextSkilltreeIdx++;
-            }
-        }
-
-
-        if (CanSkillTree) answer++;
-
-        nextSkilltreeIdx = 0;
-    }
-
+    int answer = -1;
     return answer;
 }
